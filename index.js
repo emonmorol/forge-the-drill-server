fdb1b590ea3d8ef9a899895f9c9afed4716fe3e8
emonmorol <ekramulhasane69@gmail.com>
2022-05-22 13:13:58 +0600

    getting drills api

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@forgethedrill.6ry4r.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const toolCollection = client.db("forge-the-drill").collection("drills");
+
+    app.get("/drill", async (req, res) => {
+      const drills = await toolCollection.find(query).toArray();
+      res.send(drills);
+    });
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Forge The Drill");
